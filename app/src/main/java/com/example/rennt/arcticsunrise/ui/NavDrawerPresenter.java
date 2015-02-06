a521db54175f7f729e3c76569a1c7d5073adb49f
Tom Renn <tomrenn@gmail.com>
2015-02-05 19:35:38 -0500

    simple login dialog snippet

--- app/src/main/java/com/example/rennt/arcticsunrise/ui/NavDrawerPresenter.java
+++ app/src/main/java/com/example/rennt/arcticsunrise/ui/NavDrawerPresenter.java
@@ -1,9 +1,15 @@
 package com.example.rennt.arcticsunrise.ui;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.rennt.arcticsunrise.R;
@@ -12,7 +18,9 @@ import com.example.rennt.arcticsunrise.data.api.models.User;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 import rx.functions.Action1;
+import timber.log.Timber;
 
 import static butterknife.ButterKnife.findById;
 
@@ -23,6 +31,7 @@ import static butterknife.ButterKnife.findById;
  */
 public class NavDrawerPresenter {
     private final ViewGroup rootNav;
+    private final Context context;
     private final UserManager userManager;
     private final LayoutInflater inflater;
 
@@ -32,6 +41,7 @@ public class NavDrawerPresenter {
 
     public NavDrawerPresenter(final ViewGroup rootView, final UserManager userManager){
         this.rootNav = rootView;
+        this.context = rootView.getContext();
         this.userManager = userManager;
         inflater = LayoutInflater.from(rootView.getContext());
         // setup on click buttons, etc.
@@ -49,6 +59,35 @@ public class NavDrawerPresenter {
             });
         }
         // todo: login button must have ability to display a login dialog with it's own presenter
+    }
+
+    @OnClick(R.id.sign_in) public void showLoginDialog(){
+        AlertDialog.Builder loginBuilder = new AlertDialog.Builder(rootNav.getContext());
+
+        final EditText emailField = new EditText(context);
+        emailField.setHint("Email Address");
+        EditText passwordField = new EditText(context);
+        passwordField.setInputType(EditorInfo.TYPE_TEXT_VARIATION_PASSWORD);
+        LinearLayout fieldContainer = new LinearLayout(context);
+        fieldContainer.setOrientation(LinearLayout.VERTICAL);
+        fieldContainer.addView(emailField);
+        fieldContainer.addView(passwordField);
+
+        loginBuilder.setTitle("Sign in to your WSJ Account")
+                .setView(fieldContainer)
+                .setPositiveButton(R.string.sign_in, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // todo: call UserManager login observable
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .show();
     }
 
     public void displayUserBanner(User user){
