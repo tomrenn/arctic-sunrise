4b7b15d2126079c8e3ceb01d577f30888bd84d73
Tom Renn <tomrenn@gmail.com>
2015-02-07 20:16:01 -0500

    first real user manager test!

--- app/src/test/java/com/example/rennt/arcticesunrise/UserManagerTest.java
+++ app/src/test/java/com/example/rennt/arcticesunrise/UserManagerTest.java
@@ -8,11 +8,14 @@ import android.test.mock.MockApplication;
 import android.test.mock.MockContext;
 
 import com.example.rennt.arcticsunrise.data.api.UserManager;
+import com.example.rennt.arcticsunrise.data.api.models.User;
 import com.example.rennt.arcticsunrise.data.prefs.LongPreference;
 import com.example.rennt.arcticsunrise.ui.IssueViewPagerAdapter;
 import com.squareup.okhttp.OkHttpClient;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
@@ -21,21 +24,47 @@ import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 
 
 public class UserManagerTest {
     UserManager userManager;
+    Long fakeUserId = 3l;
+    User fakeUser = new User("TestUser", "TestUser", "example@test.com", true);
 
     @Before
     public void setup(){
-        SharedPreferences prefs = mock(SharedPreferences.class);
-        this.userManager = new UserManager(new LongPreference(prefs, "test"), new OkHttpClient());
+        LongPreference userPref = mock(LongPreference.class);
+        this.userManager = new UserManager(userPref, new OkHttpClient());
     }
 
+    private void setTestUser() {
+        try {
+            Field userField = UserManager.class.getDeclaredField("user");
+            userField.setAccessible(true);
+            userField.set(userManager, fakeUser);
+        } catch (IllegalAccessException|NoSuchFieldException exception) {
+            fail(exception.toString());
+        }
+    }
+
+    /**
+     * Tests .getUser() and .isUserLoaded() based on private 'user' state.
+     */
     @Test
-    public void testUserObservable(){
-        assertTrue(false);
-        IssueViewPagerAdapter.SectionRecyclerFragment frag = new IssueViewPagerAdapter.SectionRecyclerFragment();
+    public void testUserAvailability(){
+        assertNull(userManager.getUser());
+        assertFalse(userManager.isUserLoaded());
+
+        setTestUser();
+        assertEquals(fakeUser, userManager.getUser());
+        assertTrue(userManager.isUserLoaded());
+
+        userManager.logout();
+        assertNull(userManager.getUser());
+        assertFalse(userManager.isUserLoaded());
     }
 }
